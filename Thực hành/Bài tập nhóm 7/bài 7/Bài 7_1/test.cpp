e36a4861593c819ef7cc415ab533d3d3b9e42f16
Phan Nhật Tân <162537750+nhattan2005@users.noreply.github.com>
2024-11-26 00:32:49 +0700

    Update test.cpp

--- Thực hành/Bài tập nhóm 7/bài 7/Bài 7_1/test.cpp
+++ Thực hành/Bài tập nhóm 7/bài 7/Bài 7_1/test.cpp
@@ -7,32 +7,38 @@ int main() {
     ThoiGian tg1;
     cout << "Default constructor: " << tg1 << endl;
 
+    cout << endl << "-------------------------------------------" << endl << endl;
     // Test parameterized constructor with valid values
     ThoiGian tg2(12, 30, 45);
     cout << "Parameterized constructor with valid input: " << tg2 << endl;
 
+    cout << endl << "-------------------------------------------" << endl << endl;
     // Test parameterized constructor with invalid values
     ThoiGian tg3(25, 70, -5);
     cout << "Parameterized constructor with invalid input: " << tg3 << endl;
 
+    cout << endl << "-------------------------------------------" << endl << endl;
     // Test input operator (valid input)
     ThoiGian tg4;
     cout << "Enter a valid time (hh mm ss): ";
     cin >> tg4;
     cout << "After input (valid): " << tg4 << endl;
 
+    cout << endl << "-------------------------------------------" << endl << endl;
     // Test input operator (invalid input)
     ThoiGian tg5;
     cout << "Enter an invalid time (hh mm ss): ";
     cin >> tg5;
     cout << "After input (invalid): " << tg5 << endl;
 
+    cout << endl << "-------------------------------------------" << endl << endl;
     // Test decrement operator
-    ThoiGian tg6(0, 0, 1);
+    ThoiGian tg6(0, 0, 5);
     cout << "Initial time: " << tg6 << endl;
     --tg6;
     cout << "After decrement: " << tg6 << endl;
 
+    cout << endl << "-------------------------------------------" << endl << endl;
     // Test decrement operator on time that results in invalid state
     ThoiGian tg7(0, 0, 0);
     cout << "Initial time (at zero): " << tg7 << endl;
